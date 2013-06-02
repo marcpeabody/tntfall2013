506a8fda0a9c819ee3cd5901cdbd230cf01e610e
Marc Peabody <marcpeabody@yahoo.com>
2013-06-01 22:33:25 -0400

    long run times and locations

--- js/tnt-schedules.js
+++ js/tnt-schedules.js
@@ -11,7 +11,7 @@ var setupTNTSchedules = function () {
     TNTSchedule.HalfMarathon = {};
 
     TNTSchedule.FullMarathon = {
-        "6/15": {event: "Season Kickoff"},
+        "6/15": {event: "Season Kickoff Location Alysia? 8:00am"},
 
         // Week 1
         "6/16": {fin: rest},
@@ -29,7 +29,7 @@ var setupTNTSchedules = function () {
         "6/26": {fin: crosstrain},
         "6/27": {fin: "30 minutes", pr: "5 at tempo"},
         "6/28": {fin: rest, pr: "3 miles EZ"},
-        "6/29": {fin: "11 miles<br/> Flat Place 8:00am"},
+        "6/29": {fin: "11 miles<br/> Antrim Deck 8:00am"},
 
         // Week 3
         "6/30": {fin: rest},
@@ -38,7 +38,7 @@ var setupTNTSchedules = function () {
         "7/3": {fin: crosstrain},
         "7/4": {fin: "3 miles", pr: "at race pace", event: "Colonial Hills 5K"},
         "7/5": {fin: rest, pr: "3 miles EZ"},
-        "7/6": {fin: "8 miles<br/> Hilly Place 8:00am"},
+        "7/6": {fin: "8 miles<br/> Sharon Woods 8:00am"},
 
         // Week 4
         "7/7": {fin: rest},
@@ -47,7 +47,7 @@ var setupTNTSchedules = function () {
         "7/10": {fin: crosstrain},
         "7/11": {fin: "35 minutes", pr: "6 at tempo"},
         "7/12": {fin: rest, pr: "3 miles EZ"},
-        "7/13": {fin: "13 miles<br/> Flat Place 8:00am"},
+        "7/13": {fin: "13 miles<br/> Antrim Deck 8:00am"},
 
         // Week 5
         "7/14": {fin: rest},
@@ -56,7 +56,7 @@ var setupTNTSchedules = function () {
         "7/17": {fin: crosstrain},
         "7/18": {fin: "3 miles", pr: "at race pace"},
         "7/19": {fin: rest, pr: "3 miles EZ"},
-        "7/20": {fin: "14 miles<br/> Flat Place 8:00am"},
+        "7/20": {fin: "14 miles<br/> Antrim Deck 8:00am"},
 
         // Week 6
         "7/21": {fin: rest},
@@ -65,7 +65,7 @@ var setupTNTSchedules = function () {
         "7/24": {fin: crosstrain},
         "7/25": {fin: "35 minutes", pr: "7 at tempo"},
         "7/26": {fin: rest, pr: "4 miles EZ"},
-        "7/27": {fin: "10 miles<br/> Hilly Place 8:00am"},
+        "7/27": {fin: "10 miles<br/> Sharon Woods 8:00am"},
 
         // Week 7
         "7/28": {fin: rest},
@@ -74,7 +74,7 @@ var setupTNTSchedules = function () {
         "7/31": {fin: crosstrain},
         "8/1": {fin: "40 minutes", pr: "8 at tempo"},
         "8/2": {fin: rest, pr: "4 miles EZ"},
-        "8/3": {fin: "16 miles<br/> Flat Place 8:00am"},
+        "8/3": {fin: "16 miles<br/> Antrim Deck 7:00am"},
 
         // Week 8
         "8/4": {fin: rest},
@@ -83,7 +83,7 @@ var setupTNTSchedules = function () {
         "8/7": {fin: crosstrain},
         "8/8": {fin: "3 miles", pr: "at race pace"},
         "8/9": {fin: rest, pr: "4 miles EZ"},
-        "8/10": {fin: "17 miles<br/> Flat Place 8:00am"},
+        "8/10": {fin: "17 miles<br/> Antrim Deck 7:00am"},
 
         // Week 9
         "8/11": {fin: rest},
@@ -92,7 +92,7 @@ var setupTNTSchedules = function () {
         "8/14": {fin: crosstrain},
         "8/15": {fin: "4 miles", pr: "at race pace"},
         "8/16": {fin: rest, pr: "4 miles EZ"},
-        "8/17": {fin: "15 miles<br/> Hilly Place 8:00am"},
+        "8/17": {fin: "15 miles<br/> Glacier Ridge 7:00am"},
 
         // Week 10
         "8/18": {fin: rest},
@@ -110,7 +110,7 @@ var setupTNTSchedules = function () {
         "8/28": {fin: crosstrain},
         "8/29": {fin: "4 miles", pr: "at race pace"},
         "8/30": {fin: rest, pr: "4 miles EZ"},
-        "8/31": {fin: "16 miles<br/> Hilly Place 8:00am"},
+        "8/31": {fin: "16 miles<br/> Antrim Deck 7:00am"},
 
         // Week 12
         "9/1": {fin: rest},
@@ -119,7 +119,7 @@ var setupTNTSchedules = function () {
         "9/4": {fin: crosstrain},
         "9/5": {fin: "45 minutes", pr: "10 at tempo"},
         "9/6": {fin: rest, pr: "4 miles EZ"},
-        "9/7": {fin: "19 miles<br/> Flat Place 8:00am"},
+        "9/7": {fin: "19 miles<br/> Antrim Deck 7:00am"},
 
         // Week 13
         "9/8": {fin: rest},
@@ -128,7 +128,7 @@ var setupTNTSchedules = function () {
         "9/11": {fin: crosstrain},
         "9/12": {fin: "45 minutes", pr: "10 at tempo"},
         "9/13": {fin: rest, pr: "4 miles EZ"},
-        "9/14": {fin: "20 miles<br/> Flat Place 8:00am"},
+        "9/14": {fin: "20 miles<br/> Antrim Deck 7:00am"},
 
         // Week 14
         "9/15": {fin: rest},
@@ -137,7 +137,7 @@ var setupTNTSchedules = function () {
         "9/18": {fin: crosstrain},
         "9/19": {fin: "5 miles", pr: "at race pace"},
         "9/20": {fin: rest, pr: "4 miles EZ"},
-        "9/21": {fin: "14 miles<br/> Hilly Place 8:00am"},
+        "9/21": {fin: "14 miles<br/> Sharon Woods 7:00am"},
 
         // Week 15
         "9/22": {fin: rest},
@@ -146,7 +146,7 @@ var setupTNTSchedules = function () {
         "9/25": {fin: crosstrain},
         "9/26": {fin: "40 minutes", pr: "8 at tempo"},
         "9/27": {fin: rest, pr: "4 miles EZ"},
-        "9/28": {fin: "20 miles<br/> Flat Place 8:00am"},
+        "9/28": {fin: "20 miles<br/> Antrim Deck 7:00am"},
 
         // Week 16
         "9/29": {fin: rest},
@@ -155,7 +155,7 @@ var setupTNTSchedules = function () {
         "10/2": {fin: crosstrain},
         "10/3": {fin: "30 minutes", pr: "8 at tempo"},
         "10/4": {fin: rest, pr: "4 miles EZ"},
-        "10/5": {fin: "14 miles<br/> Hilly Place 8:00am"},
+        "10/5": {fin: "14 miles<br/> Sharon Woods 7:00am"},
 
         // Week 17
         "10/6": {fin: rest},
@@ -164,7 +164,7 @@ var setupTNTSchedules = function () {
         "10/9": {fin: crosstrain},
         "10/10": {fin: "4 miles", pr: "at race pace"},
         "10/11": {fin: rest, pr: "4 miles EZ"},
-        "10/12": {fin: "8 miles<br/> Hilly Place 8:00am"},
+        "10/12": {fin: "8 miles<br/> Sharon Woods 7:00am"},
 
         // Week 18
         "10/13": {fin: rest},
